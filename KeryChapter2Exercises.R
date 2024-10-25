625538f067afe320668774bee8a1284ace12bb4f
hayesrebecca <rhayes7@uoregon.edu>
2024-10-25 10:49:18 -0700

    feat: added notes on bayes rule section

--- KeryChapter2Exercises.R
+++ KeryChapter2Exercises.R
@@ -11,6 +11,36 @@
 
 ## 2.5 Bayesian Inference
 
+## In Bayesian inference, we use probability directly to characterize
+## uncertainty about parameters whereas in classical inference probability is used to characterize operating
+## properties of statistical procedures.
+
+## Bayesian inference is characterized by two features: 
+## 1) posterior inference
+## 2) regarding all parameters as realizations of random variables
+
+## Bayes rule 
+## [z|y] = [y|z][z]\[y]
+## posterior = likelihood*prior/marginalization
+
+## [z|y] = conditional probability distribution of z given y (the probability of z being true given y)
+## [y|z] = conditional probability distribution of y given z (the probability of y being true given z)
+## [z] = marginal distribution of z (the probability of z being true)
+## [y] = marginal distribution of y (the probability of y being true)
+
+## we can think of this as a way of updating our prior knowledge about an unknown
+## quantity (expressed by [z]) with information obtained from the data ([y|z]).
+
+## for example, in occupancy modeling terms:
+## Pr(present|not detected) = Pr(not detected|present)*Pr(present) \ Pr(not detected)
+## the probability a species is present given the absense of detection (the posterior)
+## equals the probability that a species is not detected given being present (the likelihood)
+## multiplied by the probability that a species is present (the prior)
+## divided by the probability that a species is not detected (marginalization)
+
+
+
+
 ## 2.6 Basic Markov Chain Monte Carlo (MCMC)
 
 ## 2.7 Model Selection and Averaging
@@ -18,72 +48,6 @@
 ## 2.8 Assessment of Model Fit
 
 ## 2.9 Summary and Outlook
-
-# Simulate a covariate called vegHt for 100 sites
-set.seed(2014) # Set seed so we all get the same values of vegHt
-M <- 100 # Number of sites surveyed
-vegHt <- runif(M, 1, 3) # uniform from 1 to 3
-
-# Suppose that occupancy probability increases with vegHt
-# The relationship is described by an intercept of -3 and
-# a slope parameter of 2 on the logit scale
-beta0 <- -3
-beta1 <- 2
-psi <- plogis(beta0 + beta1*vegHt) # apply inverse logit
-# Now we go to 100 sites and observe presence or absence
-z <- rbinom(M, 1, psi)
-
-# Definition of negative log-likelihood.
-negLogLike <- function(beta, y, x) {
-  beta0 <- beta[1]
-  beta1 <- beta[2]
-  psi <- plogis(beta0 + beta1*x)
-  likelihood <- psi^y * (1-psi)^(1-y) # same as next line:
-  # likelihood <- dbinom(y, 1, psi)
-  return(-sum(log(likelihood)))
-}
-# Look at (negative) log-likelihood for 2 parameter sets
-negLogLike(c(0,0), y=z, x=vegHt)
-negLogLike(c(-3,2), y=z, x=vegHt) # Lower is better!
-# Let's minimize it formally by function minimisation
-starting.values <- c(beta0=0, beta1=0)
-opt.out <- optim(starting.values, negLogLike, y=z, x=vegHt, hessian=TRUE)
-(mles <- opt.out$par) # MLEs are pretty close to truth
-beta0 beta1
--2.539793 1.617025
-# Alternative 1: Brute-force grid search for MLEs
-mat <- as.matrix(expand.grid(seq(-10,10,0.1), seq(-10,10,0.1)))
-# above: Can vary resolution (e.g., from 0.1 to 0.01)
-nll <- array(NA, dim = nrow(mat))
-for (i in 1:nrow(mat)){
-  nll[i] <- negLogLike(mat[i,], y = z, x = vegHt)
-}
-which(nll == min(nll))
-mat[which(nll == min(nll)),]
-# Produce a likelihood surface, shown in Fig. 2-2.
-library(raster)
-r <- rasterFromXYZ(data.frame(x = mat[,1], y = mat[,2], z = nll))
-mapPalette <- colorRampPalette(rev(c("grey", "yellow", "red")))
-
-plot(r, col = mapPalette(100), main = "Negative log-likelihood",
-     xlab = "Intercept (beta0)", ylab = "Slope (beta1)")
-contour(r, add = TRUE, levels = seq(50, 2000, 100))
-# Alternative 2: Use canned R function glm as a shortcut
-(fm <- glm(z ~ vegHt, family = binomial)$coef)
-# Add 3 sets of MLEs into plot
-# 1. Add MLE from function minimisation
-points(mles[1], mles[2], pch = 1, lwd = 2)
-abline(mles[2],0) # Put a line through the Slope value
-lines(c(mles[1],mles[1]),c(-10,10))
-# 2. Add MLE from grid search
-points(mat[which(nll == min(nll)),1], mat[which(nll == min(nll)),2],
-       pch = 1, lwd = 2)
-# 3. Add MLE from glm function
-points(fm[1], fm[2], pch = 1, lwd = 2)
-
-Vc <- solve(opt.out$hessian) # Get variance-covariance matrix
-ASE <- sqrt(diag(Vc)) # Extract asymptotic SEs
-print(ASE)
 
 ## Exercises
 
